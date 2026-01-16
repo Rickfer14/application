27fd9597758df0f6307ad50c74f393755c1bef26
Rickfer14 <rickqf14@gmail.com>
2026-01-15 23:54:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ st.markdown(
 # 3. Música de YouTube (The Abyss - The Weeknd & Lana Del Rey)
 # Se muestra un reproductor pequeño para que ella pueda activar el sonido
 st.markdown("<div class='video-container'>", unsafe_allow_html=True)
-st.video("https://youtu.be/Y5slGFIDyfc?si=aRhVasf_Z3QvLOVH")
+st.video("https://youtu.be/DZg-paVWkH4?si=84WpFZTBF4aMM_3L")
 st.caption("dale plei para escuchar")
 st.markdown("</div>", unsafe_allow_html=True)
 
